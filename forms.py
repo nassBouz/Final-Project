404a32d7e09672965eb7ffe20f7280a2b72b85f6
nassBouz <bouzianenassima@gmail.com>
2019-04-06 14:43:41 -0700

    added some routes

--- forms.py
+++ forms.py
@@ -1,17 +1,24 @@
 from flask_wtf import FlaskForm as Form 
-from models import Teacher
-from models import Parent
+from models import User
 from models import Student
 from wtforms import StringField, PasswordField, TextAreaField, TextField, SubmitField, IntegerField
 from wtforms.validators import (DataRequired, Regexp, ValidationError, Email, Length, EqualTo)
 
 def name_exists(form, field):
-    if Teacher.select().where(Teacher.username == field.data).exists():
+    if Teacher.select().where(User.username == field.data).exists():
         raise ValidationError('Teacher with that name already exists.')
 
 class SignInForm(Form):
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
+
+class MessageForm(Form):
+    text = TextAreaField("Content", validators=[DataRequired()])
+    title = TextAreaField("Title",  validators=[DataRequired()])
+    imageUrl = TextAreaField("image here", default="noImage")
+    imageFile = TextAreaField("image file here")
+    submit = SubmitField('Create Message')
+
 
 class StudentEditForm(Form):
     email = StringField('Email', validators=[DataRequired(), Email()])
@@ -19,6 +26,8 @@ class StudentEditForm(Form):
     fullname= StringField('Full Name',validators=[ DataRequired(), Length(min=2)])
     profileImgUrl = StringField("Image")
 
+
+# EditMessageForm
 # class CreateMessageForm(form):
 # class EditMessageForm(Form):
 # class EditStudentLevel(Form):
