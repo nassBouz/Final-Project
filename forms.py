fdf95659aba36e40f161c6c809e280eeb358784f
nassBouz <bouzianenassima@gmail.com>
2019-04-10 11:57:33 -0700

    added a form to edit student

--- forms.py
+++ forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm as Form 
 from models import User
 from models import Student
+from models import Message
 from wtforms import StringField, PasswordField, TextAreaField, TextField, SubmitField, IntegerField
 from wtforms.validators import (DataRequired, Regexp, ValidationError, Email, Length, EqualTo)
 
@@ -14,18 +15,28 @@ class SignInForm(Form):
 
 class MessageForm(Form):
     text = TextAreaField("Content", validators=[DataRequired()])
-    title = TextAreaField("Title",  validators=[DataRequired()])
+    title = TextField("Title",  validators=[DataRequired()])
     imageUrl = TextAreaField("image here", default="noImage")
     imageFile = TextAreaField("image file here")
     submit = SubmitField('Create Message')
 
 
-class StudentEditForm(Form):
+class UserEditForm(Form):
     email = StringField('Email', validators=[DataRequired(), Email()])
     username = StringField('Username', validators=[DataRequired(), Regexp( r'^[a-zA-Z0-9_]+$', message=("Username should be one word, letters, numbers, and underscores only.")),name_exists])
     fullname= StringField('Full Name',validators=[ DataRequired(), Length(min=2)])
     profileImgUrl = StringField("Image")
 
+class EditStudent(Form):
+    medicalNeedds = TextAreaField("medical needs",  validators=[DataRequired()])
+    fullname = StringField('Full Name',validators=[ DataRequired(), Length(min=2)])
+    studentLevel= IntegerField(default=0)
+    workingOn = TextAreaField()
+    phonenumber = IntegerField()
+
+class EditStudentParent(Form):
+    medicalNeeds = TextAreaField("medical needs",  validators=[DataRequired()])
+    phonenumber = IntegerField()
 
 # EditMessageForm
 # class CreateMessageForm(form):
